535269c87a206ad227921bfc7b7306c2e1d9c4ea
ExtraTNT <sven.hugi@swisstxt.ch>
2025-02-23 12:37:57 +0100

    wow so fancy

--- src/main/java/ch/fhnw/algd1/converters/binary/BinConverter.java
+++ src/main/java/ch/fhnw/algd1/converters/binary/BinConverter.java
@@ -2,14 +2,31 @@ package ch.fhnw.algd1.converters.binary;
 
 public class BinConverter {
 	public static String toString(int x) {
-		// TODO expect x to be in range [-128, 127], return string with 8 binary
-		// digits representing x in 2-complement
-		return "00000000";
+		var str = Integer.toBinaryString(x & 0xFF); // it's now unsigned
+
+		var zeros = new String(new char[8 - str.length()]).replace('\0', '0');
+		
+		return zeros + str;
 	}
 
 	public static int fromString(String text) {
-		// TODO expect text to contain 8 binary digits, parse to int value in
-		// 2-complement
-		return 0;
+		int value = 1;
+		int result = 0;
+		char[] chars = text.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			if (chars[chars.length - 1 - i] == '0') {
+				value *= 2;
+				continue;
+			}
+			if(i+1 == chars.length) {
+				value = -value;
+				result += value;
+				continue;
+			}
+			result += value;
+			value *= 2;
+		}
+		
+		return result;
 	}
 }
