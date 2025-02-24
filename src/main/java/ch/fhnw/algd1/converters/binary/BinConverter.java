604a647968212caf969c9db7bc3806c8f6084779
ExtraTNT <sven.hugi@swisstxt.ch>
2025-02-24 15:04:20 +0100

    now new

--- src/main/java/ch/fhnw/algd1/converters/binary/BinConverter.java
+++ src/main/java/ch/fhnw/algd1/converters/binary/BinConverter.java
@@ -1,32 +1,51 @@
 package ch.fhnw.algd1.converters.binary;
 
 public class BinConverter {
-	public static String toString(int x) {
-		var str = Integer.toBinaryString(x & 0xFF); // it's now unsigned
+public static String toString(int x) {
 
-		var zeros = new String(new char[8 - str.length()]).replace('\0', '0');
-		
-		return zeros + str;
-	}
+/*
+    var str = Integer.toBinaryString(x & 0xFF); // it's now unsigned
 
-	public static int fromString(String text) {
-		int value = 1;
-		int result = 0;
-		char[] chars = text.toCharArray();
-		for (int i = 0; i < chars.length; i++) {
-			if (chars[chars.length - 1 - i] == '0') {
-				value *= 2;
-				continue;
-			}
-			if(i+1 == chars.length) {
-				value = -value;
-				result += value;
-				continue;
-			}
-			result += value;
-			value *= 2;
-		}
-		
-		return result;
-	}
+    var zeros = new String(new char[8 - str.length()]).replace('\0', '0');
+
+    return zeros + str;
+    */
+    
+ 
+    StringBuilder value = new StringBuilder();
+    if (x < 0) x += 256;
+    
+    int i = 1 << 7;
+    while (i >= 1) {
+        if (x >= i) {
+            value.append("1");
+            x -= i;
+        } else {
+            value.append("0");
+        }
+        i /= 2;
+    }
+    return value.toString();
+}
+
+public static int fromString(String text) {
+    int value = 1;
+    int result = 0;
+    char[] chars = text.toCharArray();
+    for (int i = 0; i < chars.length; i++) {
+        if (chars[chars.length - 1 - i] == '0') {
+            value *= 2;
+            continue;
+        }
+        if (i + 1 == chars.length) {
+            value = -value;
+            result += value;
+            continue;
+        }
+        result += value;
+        value *= 2;
+    }
+
+    return result;
+}
 }
